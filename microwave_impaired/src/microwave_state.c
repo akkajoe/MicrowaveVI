a589afdaac6355edd456637665443990163ae94f
Jibril <128831088+jibril04Afaa@users.noreply.github.com>
2026-04-13 19:28:50 -0400

    fix structure

--- microwave_impaired/src/microwave_state.c
+++ microwave_impaired/src/microwave_state.c
@@ -1,2 +1,3 @@
 #include "include/microwave_state.h"
 
+/Users/jibrilaf/Desktop/code/ATC/MicrowaveVI/microwave_impaired/include/math.h /Users/jibrilaf/Desktop/code/ATC/MicrowaveVI/microwave_impaired/include/microwave_state.h /Users/jibrilaf/Desktop/code/ATC/MicrowaveVI/microwave_impaired/include/timer.h
